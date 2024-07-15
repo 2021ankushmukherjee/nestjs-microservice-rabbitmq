a3b5586a9e4b4f969ef28e0a241308bdc9611172
ankushinfomaticae <ankush@infomaticae.com>
2024-07-15 11:21:13 +0530

    integrate swagger documentation

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3002);
+  
+  const config = new DocumentBuilder()
+  .setTitle('Auth')
+  .setDescription('Auth APi Description')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`/auth/swagger`, app, document);
+
+  const port = process.env.NODE_PORT || 3003
+  await app.listen(port,
+    () => console.log("Listening to port", port)
+  );
 }
 bootstrap();
--- apps/billing/src/main.ts
+++ apps/billing/src/main.ts
@@ -1,8 +1,31 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 import { BillingModule } from './billing.module';
+// import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(BillingModule);
-  await app.listen(3001);
+  const app = await NestFactory.create<NestExpressApplication>(BillingModule);
+  
+  app.use(express.json({ limit: '10mb' }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Billing')
+    .setDescription('Billing API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`/billing/swagger`, app, document);
+
+
+  const port = process.env.NODE_PORT || 3002
+  await app.listen(port,
+    () => console.log("Listening to port", port)
+  );
+
 }
+
 bootstrap();
